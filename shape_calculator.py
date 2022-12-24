9b4466399b6b1c89206395e50d67829450ef858f
Final Form Luigi <jacobmartinez18@gmail.com>
2022-12-24 09:22:29 -1000

    Update shape_calculator.py

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,18 +1,37 @@
 class Rectangle:
-
-    def set_width(self, width):
-        pass
+    height = 0
+    width = 0
     
-    def set_height(self, height):
-        pass
+    def __init__(self, height=0, width=0):
+        self.set_height(height)
+        self.set_width(width)
         
+    def set_width(self, width):
+        self.width = width
+        
+    def set_height(self, height):
+        self.height = height
+
     def get_area(self):
         return self.height * self.width
-        
+    
     def get_perimeter(self):
         return 2*self.width + 2*self.height
+    
+    def get_diagonal(self):
+        return (width ** 2 + height ** 2) ** .5
+    
+    def get_picture(self):
+        poly_str = ""
+        for i in range(0, self.height):
+            poly_str += f"\n{'*'*self.width}"
+        return poly_str
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super(Square, self).__init__(side,side)
         
-    def
-
-
-class Square:
+    def set_side(self, side):
+        self.set_height = side
+        self.set_width = side
